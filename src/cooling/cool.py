44e7cfabe73552d0f813bd7060a2d37263b9c3b0
opezdol <upzzdl@gmail.com>
2022-01-04 04:57:16 +0300

    made with simpler approach

--- src/cooling/cool.py
+++ src/cooling/cool.py
@@ -11,7 +11,8 @@ import cadquery as cq
 ## PARAMS
 width = 201
 length = 53
-offset = 12
+height = 250
+offset = 12 # I use it in 2 places for fix, and for sketching. Bad idea.
 fillet = 3 # actually 5
 fixOffset = 5 # mm offset from rect wire
 fixOffsetY = 16.5 # mm offset from top of rect
@@ -37,8 +38,15 @@ test_hull = (
         .select('outerBase').wires()
             # offset test print Error  corrected!! Dimentions!!
         .offset(-offset/2,mode='s',tag='innerBase')
-        .select('innerBase').vertices().fillet(fillet)
+        #.select('innerBase').vertices().fillet(fillet)
         .push(fixPnts).circle(1,mode='s')
+        )
+second_sketch = (
+        cq.Sketch()
+        # Define main geometry
+        .rect(width+offset,length+offset,mode='a', tag='outerBase')
+        .select('outerBase').wires()
+        .offset(-offset/2,mode='s',tag='innerBase')
         )
 
 tubeCut = (
@@ -61,6 +69,19 @@ result = ( cq.Workplane()
         )
 # test tubeCut
 #result = result.faces("<Y").workplane().rect(80,80).cutBlind(-190) 
-
 ##############
 
+mid_result = (
+        cq.Workplane()
+        .box(length,width,height-offset,centered=(True,True,False))
+        .faces('>Z').edges().fillet(10)
+        .faces("<Z").shell(5)
+        .faces('<Z').workplane()
+        .placeSketch(test_hull)
+        .extrude(offset)
+        .faces('<Y').edges('<Z')
+        .workplane(centerOption='CenterOfMass')
+        .placeSketch(tubeCut)
+        .cutBlind(-30)
+        )
+show_object(mid_result)
